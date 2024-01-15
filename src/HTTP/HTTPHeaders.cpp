f0ca08e0973af7a634316df0d0c39004bd0cfca8
Joachim Flottorp <joachim@flottorp.org>
2024-01-15 18:26:52 +0100

    Format testing

--- src/HTTP/HTTPHeaders.cpp
+++ src/HTTP/HTTPHeaders.cpp
@@ -7,79 +7,79 @@ constexpr auto CRLF = "\r\n";
 
 std::string ConsumeUntil(const std::string& data, u32& index, const std::string& c)
 {
-				auto startIndex = index;
-				auto endIndex = data.find(c, startIndex);
+  auto startIndex = index;
+  auto endIndex = data.find(c, startIndex);
 
-				if(endIndex == std::string_view::npos)
-				{
-								index = data.size();
-								return data.substr(startIndex);
-				}
+  if(endIndex == std::string_view::npos)
+  {
+    index = data.size();
+    return data.substr(startIndex);
+  }
 
-				index = endIndex;
-				return data.substr(startIndex, endIndex - startIndex);
+  index = endIndex;
+  return data.substr(startIndex, endIndex - startIndex);
 }
 
 bool CaseInsensitiveComparer::operator()(const std::string& lhs, const std::string& rhs) const
 {
-				// clang-format off
+  // clang-format off
     return std::lexicographical_compare(
     lhs.begin(), lhs.end(),
     rhs.begin(), rhs.end(),
     [](char a, char b) { return std::tolower(a) < std::tolower(b); }
     );
-				// clang-format on
+  // clang-format on
 }
 
 HTTPHeaders HTTPHeaders::FromString(const std::string& data)
 {
-				//    message-header = field-name ":" [ field-value ]
-				//		       field-name     = token
-				//	      field-value    = *( field-content | LWS )
-				//	      field-content  = <the OCTETs making up the field-value
-				//	      and consisting of either *TEXT or combinations
-				//	      of token, separators, and quoted-string>
+  //    message-header = field-name ":" [ field-value ]
+  //		       field-name     = token
+  //	      field-value    = *( field-content | LWS )
+  //	      field-content  = <the OCTETs making up the field-value
+  //	      and consisting of either *TEXT or combinations
+  //	      of token, separators, and quoted-string>
 
-				HTTPHeaders headers {};
-				u32 index = 0;
+  HTTPHeaders headers {};
+  u32 index = 0;
 
-				while(true)
-				{
-								// 1. Consume header until we reach CRLF.
-								auto components = ConsumeUntil(data, index, CRLF);
-								if(components.empty())
-								{
-												break;
-								}
+  while(true)
+  {
+    // 1. Consume header until we reach CRLF.
+    auto components = ConsumeUntil(data, index, CRLF);
+    if(components.empty())
+    {
+      break;
+    }
 
-								// 2. Split header into key and value.
-								auto colonIndex = components.find(':');
+    // 2. Split header into key and value.
+    auto colonIndex = components.find(':');
 
-								auto key = components.substr(0, colonIndex);
-								auto value = components.substr(colonIndex + 1);
+    auto key = components.substr(0, colonIndex);
+    auto value = components.substr(colonIndex + 1);
 
-								// 3. Trim whitespace.
-								key.erase(0, key.find_first_not_of(' '));
-								key.erase(key.find_last_not_of(' ') + 1);
+    // 3. Trim whitespace.
+    key.erase(0, key.find_first_not_of(' '));
+    key.erase(key.find_last_not_of(' ') + 1);
 
-								value.erase(0, value.find_first_not_of(' '));
-								value.erase(value.find_last_not_of(' ') + 1);
+    value.erase(0, value.find_first_not_of(' '));
+    value.erase(value.find_last_not_of(' ') + 1);
 
-								// FIXME: Should be placed somewhere else.
-								// This should denote end of headers.
-								if(key == CRLF)
-								{
-												break;
-								}
+    // FIXME: Should be placed somewhere else.
+    // This should denote end of headers.
+    if(key == CRLF)
+    {
+      break;
+    }
 
-								// 4. Add header to header store.
-								headers.Set(std::move(key), std::move(value));
+    // 4. Add header to header store.
+    headers.Set(std::move(key), std::move(value));
 
-								// 5. Increment index to skip CRLF.
-								index += 2;
-				}
+    // 5. Increment index to skip CRLF.
+    index += 2;
+  }
 
-				return headers;
+  return headers;
 }
 
 HTTPHeaders::HTTPHeaders()
@@ -88,39 +88,39 @@ HTTPHeaders::HTTPHeaders()
 
 void HTTPHeaders::Set(std::string key, std::string value)
 {
-				m_HeaderStore[std::move(key)] = std::move(value);
+  m_HeaderStore[std::move(key)] = std::move(value);
 }
 
 const std::string& HTTPHeaders::Get(const std::string& key) const
 {
-				if(auto it = m_HeaderStore.find(key); it != m_HeaderStore.end())
-				{
-								return it->second;
-				}
+  if(auto it = m_HeaderStore.find(key); it != m_HeaderStore.end())
+  {
+    return it->second;
+  }
 
-				static const std::string empty;
-				return empty;
+  static const std::string empty;
+  return empty;
 }
 
 bool HTTPHeaders::Has(const std::string& key) const
 {
-				return m_HeaderStore.find(key) != m_HeaderStore.end();
+  return m_HeaderStore.find(key) != m_HeaderStore.end();
 }
 
 std::string HTTPHeaders::ToString() const
 {
-				std::string result;
+  std::string result;
 
-				for(const auto& [key, value] : m_HeaderStore)
-				{
-								result += ToString(key);
-				}
+  for(const auto& [key, value] : m_HeaderStore)
+  {
+    result += ToString(key);
+  }
 
-				return result;
+  return result;
 }
 
 std::string HTTPHeaders::ToString(const std::string& key) const
 {
-				// Key: Value\r\n
-				return key + ": " + Get(key) + CRLF;
+  // Key: Value\r\n
+  return key + ": " + Get(key) + CRLF;
 }
--- src/HTTP/HTTPRequest.cpp
+++ src/HTTP/HTTPRequest.cpp
@@ -9,7 +9,7 @@ HTTPRequestLine::HTTPRequestLine()
 }
 
 HTTPRequestLine::HTTPRequestLine(HTTPMethod method, std::string uri, std::string version)
-				: method(method), uri(std::move(uri)), version(std::move(version))
+    : method(method), uri(std::move(uri)), version(std::move(version))
 {
 }
 
@@ -18,7 +18,7 @@ HTTPRequest::HTTPRequest()
 }
 
 HTTPRequest::HTTPRequest(HTTPRequestLine requestLine, HTTPHeaders headers, std::string body)
-				: m_RequestLine(std::move(requestLine)), m_Headers(std::move(headers)), m_Body(std::move(body))
+    : m_RequestLine(std::move(requestLine)), m_Headers(std::move(headers)), m_Body(std::move(body))
 {
 }
 
@@ -28,40 +28,40 @@ HTTPRequest::~HTTPRequest()
 
 void HTTPRequest::DebugLog() const
 {
-				std::cout << "Method: " << HTTPMethodToString(m_RequestLine.method) << std::endl;
-				std::cout << "URI: " << m_RequestLine.uri << std::endl;
-				std::cout << "Version: " << m_RequestLine.version << std::endl;
+  std::cout << "Method: " << HTTPMethodToString(m_RequestLine.method) << std::endl;
+  std::cout << "URI: " << m_RequestLine.uri << std::endl;
+  std::cout << "Version: " << m_RequestLine.version << std::endl;
 
-				std::cout << "Headers:" << std::endl;
-				for(const auto& [key, value] : m_Headers.m_HeaderStore)
-				{
-								std::cout << "  " << key << ": " << value << std::endl;
-				}
+  std::cout << "Headers:" << std::endl;
+  for(const auto& [key, value] : m_Headers.m_HeaderStore)
+  {
+    std::cout << "  " << key << ": " << value << std::endl;
+  }
 
-				if(m_Body.empty())
-								std::cout << "Body: <empty>" << std::endl;
-				else
-								std::cout << "Body: " << m_Body << std::endl;
+  if(m_Body.empty())
+    std::cout << "Body: <empty>" << std::endl;
+  else
+    std::cout << "Body: " << m_Body << std::endl;
 }
 
 std::string HTTPRequest::ToString() const
 {
-				// FIXME: Less string copies.
+  // FIXME: Less string copies.
 
-				std::string result;
+  std::string result;
 
-				result += HTTPMethodToString(m_RequestLine.method);
-				result += " ";
-				result += m_RequestLine.uri;
-				result += " ";
-				result += m_RequestLine.version;
-				result += "\r\n";
+  result += HTTPMethodToString(m_RequestLine.method);
+  result += " ";
+  result += m_RequestLine.uri;
+  result += " ";
+  result += m_RequestLine.version;
+  result += "\r\n";
 
-				result += m_Headers.ToString();
+  result += m_Headers.ToString();
 
-				result += "\r\n";
+  result += "\r\n";
 
-				result += m_Body;
+  result += m_Body;
 
-				return result;
+  return result;
 }
